14a8d3a1c8b0065970371d245643e69a9b5c644b
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-06-26 22:19:11 +0000

    loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
-export default function appendToEachArrayValue(array, appendString) { 
-   const array2 = []; 
-   for (const value of array) { 
-     array2.push(appendString + value); 
-   } 
-   return array2; 
- }
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
