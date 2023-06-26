b850ac8e596abd17cac29a5b1afe2ce2de54eee3
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-06-26 20:45:31 +0000

    budget object

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,10 @@
-export default function getBudgetObject(income, gdp, capita) { 
-   const budget = { 
-     income, 
-     gdp, 
-     capita, 
-   }; 
-  
-   return budget; 
- }
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
+
